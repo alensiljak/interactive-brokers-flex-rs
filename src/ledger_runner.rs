9d317d2f58d3da27cac30d52c1ca3d6a3345bf51
Alen Šiljak <dev@alensiljak.eu.org>
2023-01-04 12:25:44 +0100

    shell_words seem to work

--- src/ledger_runner.rs
+++ src/ledger_runner.rs
@@ -5,7 +5,6 @@
 use std::process::{Command, Output};
 
 use chrono::{Days, Local};
-use cmd_lib::run_fun;
 
 use crate::{compare::TRANSACTION_DAYS, ledger_reg_output_parser, model::CommonTransaction};
 
@@ -62,11 +61,8 @@ fn get_ledger_args(date_param: &str, ledger_init_file: Option<String>) -> Vec<St
 
 #[allow(unused)]
 fn get_ledger_cmd(date_param: &str, ledger_init_file: Option<String>) -> String {
-    let mut cmd = "ledger r".to_string();
+    let mut cmd = format!("ledger r -b {date_param} -d ");
 
-    cmd.push_str(" -b ");
-    cmd.push_str(date_param);
-    cmd.push_str(" -d ");
     cmd.push_str(r#" "(account =~ /income/ and account =~ /ib/) or (account =~ /ib/ and account =~ /withh/)""#);
 
     if crate::compare::DATE_MODE == "effective" {
@@ -83,6 +79,7 @@ fn get_ledger_cmd(date_param: &str, ledger_init_file: Option<String>) -> String 
 
 /// Runs Ledger with the given command and returns the output in lines.
 /// cmd: The ledger command to execute, without `ledger` at the beginning.
+/// Returns the lines of the Ledger output.
 #[allow(unused)]
 fn run_ledger(args: Vec<String>) -> Vec<String> {
     // cmd: &str
@@ -116,13 +113,16 @@ pub fn get_ledger_tx(ledger_init_file: Option<String>) -> Vec<CommonTransaction>
 
     let cmd = get_ledger_cmd(&date_param, ledger_init_file);
     // let args = get_ledger_args(&date_param, ledger_init_file);
+    let args = shell_words::split(&cmd).expect("command args");
 
     log::debug!("running: {}", cmd);
 
-    // let mut output = run_ledger(args);
-    let output = get_ledger_tx_cmdlib(&cmd);
+    let output = run_ledger(args);
 
-    let lines: Vec<&str> = output.lines().collect();
+    //let lines: Vec<&str> = output.lines().collect();
+    let lines = output
+        .iter().map(|item| item.as_str())
+        .collect();
 
     // cleanup
     let clean_lines = ledger_reg_output_parser::clean_up_register_output(lines);
@@ -133,14 +133,37 @@ pub fn get_ledger_tx(ledger_init_file: Option<String>) -> Vec<CommonTransaction>
     txs
 }
 
-fn get_ledger_tx_cmdlib(cmd: &str) -> String {
-    // run_fun!(
-    //     ledger r -b ${start_date} -d  "(account =~ /income/ and account =~ /ib/) or (account =~ /ib/ and account =~ /withh/)" --init-file ${init_file}
-    // ).unwrap()
+// fn get_ledger_tx_cmdlib(cmd: &str) -> String {
+//     // run_fun!(
+//     //     ledger r -b ${start_date} -d  "(account =~ /income/ and account =~ /ib/) or (account =~ /ib/ and account =~ /withh/)" --init-file ${init_file}
+//     // ).unwrap()
+//     // run_fun!($cmd).unwrap()
+//     run_fun!(${cmd}).unwrap()
+// }
 
-    // append the command at the beginning?
+/// Runs Ledger in a shell.
+/// Should handle the command-as-string well.
+#[allow(unused)]
+fn run_ledger_in_shell(cmd: &str) -> Output {
+    log::debug!("running in shell: {}", cmd);
 
-    run_fun!($cmd).unwrap()
+    let output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .args(["/C", cmd])
+            // .arg("/C")
+            // .arg(cmd)
+            .output()
+            .expect("failed to execute process")
+    } else {
+        Command::new("sh")
+            .arg("-c")
+            // .arg("echo hello")
+            .arg(cmd)
+            .output()
+            .expect("failed to execute process")
+    };
+
+    output
 }
 
 ////
@@ -149,11 +172,9 @@ fn get_ledger_tx_cmdlib(cmd: &str) -> String {
 
 #[cfg(test)]
 mod tests {
-    use cmd_lib::run_cmd;
-    use cmd_lib::run_fun;
-
     use super::get_ledger_tx;
     use super::run_ledger;
+    use crate::ledger_runner::run_ledger_in_shell;
     use crate::test_fixtures::*;
 
     /// Confirm that Ledger can be invoked from the current directory.
@@ -193,37 +214,107 @@ mod tests {
         assert_eq!(7, actual.len());
     }
 
-    /// See if a command can be run with cmd_lib.
-    #[test_log::test]
-    fn test_cmd_lib() {
-        let success = run_cmd!(dir);
-        assert!(success.is_ok());
+    // /// See if a command can be run with cmd_lib.
+    // #[test_log::test]
+    // fn test_cmd_lib() {
+    //     let success = run_cmd!(dir);
+    //     assert!(success.is_ok());
+    //     let actual = run_fun!(dir).unwrap();
+    //     log::debug!("actual is: {}", actual);
+    //     assert_ne!(String::default(), actual);
+    // }
 
-        let actual = run_fun!(dir).unwrap();
-        log::debug!("actual is: {}", actual);
-        assert_ne!(String::default(), actual);
+    // /// Proof of concept of running the full command line with ledger parameters.
+    // /// This works as expected.
+    // #[test_log::test]
+    // fn test_complex_cmdlib() {
+    //     let result = run_fun!(
+    //         ledger r -b 2022-03-01 -d  "(account =~ /income/ and account =~ /ib/) or (account =~ /ib/ and account =~ /withh/)" --init-file tests/init.ledger
+    //     );
+    //     let actual = match result {
+    //         Ok(output) => output,
+    //         Err(e) => panic!("error: {e}"),
+    //     };
+    //     log::debug!("result: {}", actual);
+    //     // Asserts
+    //     assert_ne!(String::default(), actual);
+    //     let mut expected = String::default();
+    //     expected += "2022-12-15 TRET Distribution                  Income:Investment:IB:TRET_AS                      -38.40 EUR           -38.40 EUR\r\n";
+    //     expected += "                                              Expenses:Investment:IB:Withholding Tax              5.77 EUR           -32.63 EUR\r";
+    //     assert_eq!(expected, actual);
+    // }
+
+    // /// This does not work so well.
+    // #[test_log::test]
+    // fn test_sending_string_cmdlib() {
+    //     let cmd = r#"ledger r -b 2022-03-01 -d  "(account =~ /income/ and account =~ /ib/) or (account =~ /ib/ and account =~ /withh/)" --init-file tests/init.ledger"#;
+    //     let actual = super::get_ledger_tx_cmdlib(cmd);
+    //     log::debug!("result: {}", actual);
+    //     assert!(false);
+    // }
+
+    // /// Does not work due to string. Effectively the same as run_fun.
+    // #[test_log::test]
+    // fn test_cmdlib_spawn() {
+    //     let cmd = r#"ledger r -b 2022-03-01 -d  "(account =~ /income/ and account =~ /ib/) or (account =~ /ib/ and account =~ /withh/)" --init-file tests/init.ledger"#;
+    //     let mut process = spawn_with_output!(${cmd}).unwrap();
+    //     let output = process.wait_with_output().unwrap();
+    //     log::debug!("output: {:}", output);
+    //     assert!(false);
+    // }
+
+    // commandspec interprets the string command wrong.
+    // #[test_log::test]
+    // fn test_commandspec_string() {
+    //     let cmd = r#"ledger r -b 2022-03-01 -d  "(account =~ /income/ and account =~ /ib/) or (account =~ /ib/ and account =~ /withh/)" --init-file tests/init.ledger"#;
+    //     let output = command!("{cmd}").unwrap().output().unwrap();
+    //     let result = output.stdout;
+    //     let actual = String::from_utf8(result).unwrap();
+    //     let error = String::from_utf8(output.stderr).unwrap();
+    //     log::debug!("actual: {}, error: {:?}", actual, error);
+    //     assert!(false);
+    // }
+
+    /// The same problem as with cmdlib.
+    #[test_log::test]
+    fn test_shell() {
+        let cmd = r#"ledger r -b 2022-03-01 -d "(account =~ /income/ and account =~ /ib/) or (account =~ /ib/ and account =~ /withh/)" --init-file tests/init.ledger"#;
+        // let cmd = "ledger r -b 2022-03-01 -d  \"(account =~ /income/ and account =~ /ib/) or (account =~ /ib/ and account =~ /withh/)\" --init-file tests/init.ledger";
+        let output = run_ledger_in_shell(cmd);
+
+        let hello = String::from_utf8(output.stdout).unwrap();
+        log::debug!("stdout: {}", hello);
+
+        let error = String::from_utf8(output.stderr).unwrap();
+        log::debug!("error: {}", error);
+
+        assert!(error.is_empty());
+        assert!(false);
     }
 
-    /// Proof of concept of running the full command line with ledger parameters.
     #[test_log::test]
-    fn test_complex_cmdlib() {
-        let result = run_fun!(
-            ledger r -b 2022-03-01 -d  "(account =~ /income/ and account =~ /ib/) or (account =~ /ib/ and account =~ /withh/)" --init-file tests/init.ledger
-        );
-        let actual = match result {
-            Ok(output) => output,
-            Err(e) => panic!("error: {e}")
-        };
+    fn test_shellwords() {
+        let cmd = r#"ledger r -b 2022-03-01 -d "(account =~ /income/ and account =~ /ib/) or (account =~ /ib/ and account =~ /withh/)" --init-file tests/init.ledger"#;
+        let actual = shell_words::split(cmd).unwrap();
 
-        log::debug!("result: {}", actual);
+        log::debug!("result: {:?}", actual);
 
-        // Asserts
+        assert!(!actual.is_empty());
+        assert_eq!(8, actual.len());
+    }
 
-        assert_ne!(String::default(), actual);
+    /// Run the complex query on Ledger, using shell-words.
+    #[test_log::test]
+    fn test_ledger_words() {
+        let cmd = r#"r -b 2022-03-01 -d  "(account =~ /income/ and account =~ /ib/) or (account =~ /ib/ and account =~ /withh/)" --init-file tests/init.ledger"#;
+        let args = shell_words::split(cmd).unwrap();
 
-        let mut expected = String::default();
-        expected += "2022-12-15 TRET Distribution                  Income:Investment:IB:TRET_AS                      -38.40 EUR           -38.40 EUR\r\n";
-        expected += "                                              Expenses:Investment:IB:Withholding Tax              5.77 EUR           -32.63 EUR\r";
+        let actual = run_ledger(args);
+
+        let expected: Vec<&str> = r#"2022-12-15 TRET Distribution                  Income:Investment:IB:TRET_AS                      -38.40 EUR           -38.40 EUR
+                                              Expenses:Investment:IB:Withholding Tax              5.77 EUR           -32.63 EUR"#.lines().collect();
+
+        assert!(!actual.is_empty());
         assert_eq!(expected, actual);
     }
 }
